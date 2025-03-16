9a7723b165c59231809ea56c82c28cb5895c16fe
Luna Midori <jaredteam@gmail.com>
2025-03-16 10:37:55 -0700

    Refactor UI layout in main.py to use columns for better organization of backend management settings

--- main.py
+++ main.py
@@ -104,12 +104,12 @@ with ui.row():
         with ui.row():
             ui.label("Manage Backends:")
 
-        with ui.row():
+        with ui.column():
 
-            ui.label("Set Backend Port Number:")
+            ui.label("Settings:")
             ui.input(label='Port Number', placeholder='Edit to change port', on_change=lambda e: manager.change_port(e.value))
 
-        with ui.row():
+        with ui.column():
 
             ui.label("LRM / LLM Backends:")
 
@@ -121,7 +121,7 @@ with ui.row():
                 ui.label("Ollama:")
                 ui.button("Ollama", on_click=lambda: ollama(ui, manager, docker_run_command))
 
-        with ui.row():
+        with ui.column():
 
             ui.label("Chat WebUi:")
 
@@ -134,8 +134,8 @@ with ui.row():
                 ui.button("Big-AGI V1 (Stable)", on_click=lambda: bigagi(ui, manager, docker_run_command))
                 ui.button("Big-AGI V2 (Beta)", on_click=lambda: bigagi_two(ui, manager, docker_run_rm_command, docker_sock_command))
 
-            with ui.column():
-                ui.label("Mozilla AI (Blueprints):")
+        with ui.column():
+            ui.label("Mozilla AI (Blueprints):")
 
         #ui.button("3 - Update Backends in Subsystem", on_click=on_button_click)
         #ui.button("4 - Uninstall Backends from Subsystem", on_click=on_button_click)
