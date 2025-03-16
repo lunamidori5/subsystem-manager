10389d8e903d2ddcc731e5440a886cb415d9df66
Luna Midori <jaredteam@gmail.com>
2025-03-16 10:54:29 -0700

    Add invokeai function for managing Docker commands in the UI

---
+++ invokeai.py
@@ -0,0 +1,44 @@
+
+from runcommand import run_commands_async
+
+async def invokeai(ui, manager, docker_run_command):
+    n = ui.notification(timeout=None)
+    n.message = f'Starting... Please wait...'
+    manager.change_image("midori-ai-invokeai")
+    n.spinner = True
+
+    if manager.port == 30000:
+        local_port_offset = 9090
+    else:
+        local_port_offset = 0
+    
+    port_to_use = manager.port + local_port_offset
+    full_image_name_command = f"--name {manager.image}"
+
+    mount_folders = "-v /var/lib/docker/volumes/midoriai_midori-ai-invokeai/_data:/invokeai"
+
+    docker_command = f"{manager.command_base} {docker_run_command} -d {mount_folders} -p {port_to_use}:9090"
+
+    print(manager.type)
+
+    command_pre_list = []
+
+    if "Purge".lower() in manager.type.lower():
+        command_pre_list.append(f"{manager.command_base} stop {manager.image}")
+        command_pre_list.append(f"{manager.command_base} rm {manager.image}")
+
+    if "Start".lower() in manager.type.lower():
+        command_pre_list.append(f"{manager.command_base} start {manager.image}")
+
+    if "Shutdown".lower() in manager.type.lower():
+        command_pre_list.append(f"{manager.command_base} stop {manager.image}")
+    
+    if "Install".lower() in manager.type.lower():
+        if manager.use_gpu:
+            command_pre_list.append(f"{docker_command} --gpus all {full_image_name_command} -ti ghcr.io/invoke-ai/invokeai")
+        else:
+            command_pre_list.append(f"{docker_command} {full_image_name_command} -ti ghcr.io/invoke-ai/invokeai")
+        
+    await run_commands_async(n, command_pre_list)
+
+    manager.reset_image()
